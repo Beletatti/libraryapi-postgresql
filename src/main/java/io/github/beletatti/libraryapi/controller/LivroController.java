0fccb4f7c462b817521611f3d3114394b085d75c
Beletatti <kaiquebeletatti@gmail.com>
2025-05-27 10:51:13 -0300

    mapstruct config install

--- src/main/java/io/github/beletatti/libraryapi/controller/LivroController.java
+++ src/main/java/io/github/beletatti/libraryapi/controller/LivroController.java
@@ -7,6 +7,7 @@ import io.github.beletatti.libraryapi.service.LivroService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class LivroController {
 
     private final LivroService service;
 
+    @PostMapping
     public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO dto) {
         try {
             return ResponseEntity.ok(dto);
